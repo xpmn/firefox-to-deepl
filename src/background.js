0cd01fe0633d6adc55e32c9391f5878490c4c11b
zznidar <zznidar@users.noreply.github.com>
2021-05-22 13:12:11 +0200

    Escape pipe and backslash with backslashž

--- src/background.js
+++ src/background.js
@@ -18,7 +18,7 @@ browser.contextMenus.create({
 browser.contextMenus.onClicked.addListener((info, tab) => {
   switch (info.menuItemId) {
     case 'translate-text':
-      const translateURL = `${deeplURL + defaultLang}/${encodeURIComponent(info.selectionText).replaceAll("%2F", "\\%2F")}`;
+      const translateURL = `${deeplURL + defaultLang}/${encodeURIComponent(info.selectionText).replaceAll("%2F", "\\%2F").replaceAll("%7C", "\\%7C").replaceAll("%5C", "%5C%5C")}`;
       const querying = browser.tabs.query({ currentWindow: true, active: true });
       querying.then((current) => {
         if (current.length) {
@@ -42,7 +42,7 @@ browser.commands.onCommand.addListener(async command => {
     case 'translate-text':
       const { id, index } = (await browser.tabs.query({ active: true, currentWindow: true }))[0];
       const text = (await browser.tabs.executeScript(id, { code: 'getSelection()+""', }))[0];
-      const translateURL = `${deeplURL + defaultLang}/${encodeURIComponent(text).replaceAll("%2F", "\\%2F")}`;
+      const translateURL = `${deeplURL + defaultLang}/${encodeURIComponent(text).replaceAll("%2F", "\\%2F").replaceAll("%7C", "\\%7C").replaceAll("%5C", "%5C%5C")}`;
       browser.tabs.create({
         url: translateURL,
         active: true,
