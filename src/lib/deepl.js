24b9890399886d66f101f2d26e9d6d393fc9ad99
Kevin Ulrich <kulrich@ulrich.im>
2022-02-06 17:47:20 +0100

    Implement settings-based popup translation

---
+++ src/lib/deepl.js
@@ -0,0 +1,43 @@
+async function translate(sourceText, targetLanguage) {
+    let apiUrl = "https://api.deepl.com/v2/translate";
+
+    if(await getOption("apiType") === "free") {
+        apiUrl = "https://api-free.deepl.com/v2/translate";
+    }
+
+    let apiKey = await getOption("apiKey");
+
+    if(typeof targetLanguage !== "string") {
+        targetLanguage = await getOption("defaultLang");
+    }
+
+    const query = new URLSearchParams({
+        auth_key: apiKey,
+        text: sourceText,
+        target_lang: targetLanguage,
+    });
+
+    const response = await fetch(apiUrl, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: query.toString()
+    });
+
+    const data = await response.json();
+
+    if(typeof data.translations === 'undefined'
+        || typeof data.translations[0] === 'undefined'
+        || typeof data.translations[0].detected_source_language !== 'string'
+        || typeof data.translations[0].text !== 'string') {
+        throw "No translation available";
+    }
+
+    return {
+        sourceLanguage: data.translations[0].detected_source_language,
+        text: data.translations[0].text
+    }
+
+}
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,4 +1,4 @@
-browser.runtime.sendMessage("Handshake");
+browser.runtime.sendMessage("handshake");
 
 browser.runtime.onMessage.addListener(function(message,sender,sendResponse){
     document.body.classList.remove("error");
@@ -18,37 +18,3 @@ browser.runtime.onMessage.addListener(function(message,sender,sendResponse){
         })
 
 });
-
-async function translate(sourceText) {
-    const apiUrl = "https://api-free.deepl.com/v2/translate";
-
-    const query = new URLSearchParams({
-        auth_key: "c90827b5-6869-758c-17ed-9f423c264b7c:fx",
-        text: sourceText,
-        target_lang: "DE",
-    });
-
-    const response = await fetch(apiUrl, {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: query.toString()
-    });
-
-    const data = await response.json();
-
-    if(typeof data.translations === 'undefined'
-        || typeof data.translations[0] === 'undefined'
-        || typeof data.translations[0].detected_source_language !== 'string'
-        || typeof data.translations[0].text !== 'string') {
-        throw "No translation available";
-    }
-
-    return {
-        sourceLanguage: data.translations[0].detected_source_language,
-        text: data.translations[0].text
-    }
-
-}
